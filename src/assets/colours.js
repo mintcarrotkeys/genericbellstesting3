7dbdc88dfdfe33109e0485d2fe07053cca294e71
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-03-13 21:20:22 +1100

    1.5.1

--- src/assets/colours.js
+++ src/assets/colours.js
@@ -1,14 +1,47 @@
 
 export const colourPack = [
-    {hex: "#92c748", isDark: false},
-    {hex: "#efc94f", isDark: false},
-    {hex: "#68babe", isDark: false},
+    {hex: "#97CB4D", isDark: false},
+    {hex: "#57BBC1", isDark: false},
+    {hex: "#BBB3E5", isDark: false},
     {hex: "#eeaab7", isDark: false},
-    {hex: "#ff9100", isDark: false},
-    {hex: "#a51221", isDark: true},
-    {hex: "#144766", isDark: true},
-    {hex: "#462456", isDark: true},
-    {hex: "#65350f", isDark: true},
+    {hex: "#F49B26", isDark: false},
+    {hex: "#F4C325", isDark: false},
+    {hex: "#28631D", isDark: true},
+    {hex: "#0D4C73", isDark: true},
+    {hex: "#66367D", isDark: true},
+    {hex: "#9E2E66", isDark: true},
+    {hex: "#A12512", isDark: true},
+    {hex: "#6C3B13", isDark: true},
     {hex: "#333333", isDark: true},
-    {hex: "#d0d0d0", isDark: false}
-]
+    {hex: "#CCCCCC", isDark: false}
+];
+
+
+
+/***
+ * :root {
+ *     --cf0: #000000;
+ *     --cf1: #222222;
+ *     --cf2: #333333;
+ *     --cf3: #666666;
+ *     --cf4: #d0d0d0;
+ *
+ *
+ *     --back2: #e0e0e0;
+ *     --back1: #eeeeee;
+ *     --back0: #ffffff;
+ *
+ *     --green: #97CB4D / #95C059
+ *     --yellow: #F4C325 / #E8BD31
+ *
+ *     --c-nav: #ffffff /
+ *     --c-cards: #ffffff /
+ *
+ *     --button-text: #ffffff /
+ *     --button-back: #333333 /
+ *
+ * } *
+ *
+ *
+ *
+ * ***/
