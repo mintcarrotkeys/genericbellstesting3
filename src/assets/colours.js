c782b7a2c312ded99fe2ebdddf6aaab3e00d8cca
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-03-15 20:07:35 +1100

    Update colours.js

--- src/assets/colours.js
+++ src/assets/colours.js
@@ -1,16 +1,16 @@
 
 export const colourPack = [
     {hex: "#97CB4D", isDark: false},
+    {hex: "#F4C325", isDark: false},
     {hex: "#57BBC1", isDark: false},
     {hex: "#0D4C73", isDark: true},
-    {hex: "#F4C325", isDark: false},
     {hex: "#F49B26", isDark: false},
     {hex: "#A51221", isDark: true},
     {hex: "#F0A8B6", isDark: false},
-    {hex: "#9E2E66", isDark: true},
-    {hex: "#28631D", isDark: true},
     {hex: "#582E6B", isDark: true},
     {hex: "#D5B5E6", isDark: false},
+    {hex: "#9E2E66", isDark: true},
+    {hex: "#28631D", isDark: true},
     {hex: "#C18A5C", isDark: false},
     {hex: "#333333", isDark: true},
     {hex: "#CCCCCC", isDark: false}
